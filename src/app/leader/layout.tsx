df6f17f5ecd92e524ad774baa868e904e48971a6
jwkjr <jwkjr@naver.com>
2026-05-15 00:41:00 +0900

    인도자용 아이콘 설정 오류 수정

--- src/app/leader/layout.tsx
+++ src/app/leader/layout.tsx
@@ -8,34 +8,33 @@ export const metadata: Metadata = {
     capable: true,
     statusBarStyle: "default",
     title: "구역카드(인도자용)",
-
-    icons: {
-      icon: [
-        {
-          url: "/leader-icon-192.png",
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          url: "/leader-icon-512.png",
-          sizes: "512x512",
-          type: "image/png",
-        },
-      ],
-      apple: [
-        {
-          url: "/leader-apple-touch-icon.png",
-          sizes: "180x180",
-          type: "image/png",
-        },
-      ],
-    },  
   },
   openGraph: {
     title: "구역카드(인도자용)",
     description: "후포회중 구역 방문 관리 시스템",
     siteName: "구역카드(인도자용)",
     type: "website",
+  },
+  icons: {
+    icon: [
+      {
+        url: "/leader-icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/leader-icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    apple: [
+      {
+        url: "/leader-apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
   },
 };
 
