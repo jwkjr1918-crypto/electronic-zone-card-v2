bf4950fe79ac9d5a4d673ceb419f101aba6c75d5
jwkjr <jwkjr@naver.com>
2026-05-24 15:23:24 +0900

    PWA 홈화면 아이콘 설정 추가

--- src/app/leader/layout.tsx
+++ src/app/leader/layout.tsx
@@ -4,16 +4,41 @@ export const metadata: Metadata = {
   title: "구역카드(인도자용)",
   description: "인도자용 구역 관리",
   manifest: "/leader/manifest.webmanifest",
+
   appleWebApp: {
     capable: true,
     statusBarStyle: "default",
     title: "구역카드(인도자용)",
   },
+
   openGraph: {
     title: "구역카드(인도자용)",
     description: "인도자용 구역 관리",
     siteName: "구역카드(인도자용)",
     type: "website",
+  },
+
+  icons: {
+    icon: [
+      {
+        url: "/leader-icon-192-safe.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/leader-icon-512-safe.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+
+    apple: [
+      {
+        url: "/leader-icon-192-safe.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+    ],
   },
 };
 
